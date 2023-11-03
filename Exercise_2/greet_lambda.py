3b211d7f5aa6a2f9bcc50d62de3f8e6ae8f327e0
Uchita <uchitavietnam@gmail.com>
2023-11-03 17:31:20 +0900

    update

--- Exercise_2/greet_lambda.py
+++ Exercise_2/greet_lambda.py
@@ -1,4 +1,12 @@
 import os
+import logging
+
+# Set up the logging configuration
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    return "{} from Lambda!".format(os.environ['greeting'])
+    # Log an info message
+    response = "{} from Lambda!".format(os.environ['greeting'])
+    logger.info(response)
+    return response
